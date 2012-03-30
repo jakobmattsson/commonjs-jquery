b916f19bd52be37d375337a1d792c64491c297b8
Jakob Mattsson <jakob.mattsson@gmail.com>
2012-03-30 21:48:51 +0200

    1.6.4

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cat src/jquery/jquery-1.5.2.js | awk '{print "    " $0}' > .jquery.tabbed.js
+cat src/jquery/jquery-1.6.4.js | awk '{print "    " $0}' > .jquery.tabbed.js
 cat src/before.js .jquery.tabbed.js src/after.js > commonjs-jquery.js
